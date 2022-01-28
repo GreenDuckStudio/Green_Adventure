8b4024139a75cd96967782587c3267d3194e8417
GreenDuckStudio <69764730+GreenDuckStudio@users.noreply.github.com>
2022-01-28 17:51:16 +0100

    Steam2

--- src/main/java/de/greenduck/greenadventure/block/ModBlocks.java
+++ src/main/java/de/greenduck/greenadventure/block/ModBlocks.java
@@ -2,12 +2,16 @@ package de.greenduck.greenadventure.block;
 
 import de.greenduck.greenadventure.GreenAdventure;
 import de.greenduck.greenadventure.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.OreBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,14 +23,15 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, GreenAdventure.MOD_ID);
 
-    public static final RegistryObject<Block> TITANIUM_BLOCK = registerBlock("titanium_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(12f)));
+    public static final RegistryObject<Block> AERUM_ORE = registerBlock("aerum_ore",
+            () -> new Block(BlockBehaviour.Properties
+                    .of(Material.STONE)
+                    .strength(4.5F, 3.0F)
+                    .sound(SoundType.DEEPSLATE)
+                    ), UniformInt.of(3, 7));
 
-    public static final RegistryObject<Block> TITANIUM_ORE = registerBlock("aerum_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f)));
 
-
-    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, UniformInt of) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
         return toReturn;
@@ -34,7 +39,7 @@ public class ModBlocks {
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+                new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
     }
 
     public static void register(IEventBus eventBus) {
